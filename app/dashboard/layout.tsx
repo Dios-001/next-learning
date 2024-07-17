4fba1963402d3a4b88fbb214df15fa67e3aecc6c
Dios <abhishekpatawari.codes@gmail.com>
2024-07-17 21:00:49 +0530

    working

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,8 +1,8 @@
 
-
 import SideNav from '@/app/ui/dashboard/sidenav'
 import React from 'react';
 
+export const experimental_ppr = true;
 
 export default function Layout ({children}:{children: React.ReactNode}){
 
